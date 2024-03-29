f15a437988a9797ec804e26d0a4393fc2751df4f
Back Yu <yhfszb@gmail.com>
2024-03-29 14:35:15 +0800

    pool: 如果重用对象的缓冲区过大则直接释放

--- pool.go
+++ pool.go
@@ -26,6 +26,22 @@ func (cfg *frozenConfig) ReturnStream(stream *Stream) {
 	stream.out = nil
 	stream.Error = nil
 	stream.Attachment = nil
+	// 参考 fmt 包中关于 buf 的使用
+	// fmt/print.go
+
+	//
+	// Proper usage of a sync.Pool requires each entry to have approximately
+	// the same memory cost. To obtain this property when the stored type
+	// contains a variably-sized buffer, we add a hard limit on the maximum
+	// buffer to place back in the pool. If the buffer is larger than the
+	// limit, we drop the buffer and recycle just the printer.
+	//
+	// See https://golang.org/issue/23199.
+
+	// 设置一个强限制避免对象池持有缓冲区过大的对象
+	if cap(stream.buf) > 64*1024 {
+		return
+	}
 	cfg.streamPool.Put(stream)
 }
 
